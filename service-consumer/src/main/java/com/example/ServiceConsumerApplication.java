e816b7da3aa6545ddc717b2d0dc272e4130385d5
root <lizongzai@gmail.com>
2023-02-27 12:45:33 +0800

    V7 Feign负载均衡策略-全局随机策略和局部随机策略

--- service-consumer/src/main/java/com/example/ServiceConsumerApplication.java
+++ service-consumer/src/main/java/com/example/ServiceConsumerApplication.java
@@ -1,15 +1,22 @@
 package com.example;
 
+import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 
 @EnableFeignClients
 @SpringBootApplication
 @MapperScan("com.example.mapper")
 public class ServiceConsumerApplication {
+
+  @Bean
+  public RandomRule randomRule() {
+    return new RandomRule();
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ServiceConsumerApplication.class,args);
